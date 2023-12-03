96debc9035b170cddb5ceca4a0d487eaff10aff3
Roman Trufanov <pingvin1308@gmail.com>
2023-12-03 23:49:26 +0400

    day 1

---
+++ Day1/CalibrationValues.cs
@@ -0,0 +1,9 @@
+﻿namespace AdventOfCode.Day1;
+
+public static class CalibrationValues
+{
+    public static async Task<string[]> GetValues()
+    {
+        return await File.ReadAllLinesAsync("Day1/input.txt");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCode.Day1;
+
+var calibrationValues = await CalibrationValues.GetValues();
+var finalNumbers = new List<(string CalibrationValue, int FinalNumber)>();
+
+var digits = new Dictionary<string, int>
+{
+    { "one", 1 },
+    { "1", 1 },
+    { "two", 2 },
+    { "2", 2 },
+    { "three", 3 },
+    { "3", 3 },
+    { "four", 4 },
+    { "4", 4 },
+    { "five", 5 },
+    { "5", 5 },
+    { "six", 6 },
+    { "6", 6 },
+    { "seven", 7 },
+    { "7", 7 },
+    { "eight", 8 },
+    { "8", 8 },
+    { "nine", 9 },
+    { "9", 9 },
+};
+
+foreach (var calibrationValue in calibrationValues)
+{
+    var numbersWithIndexes = new List<(int Value, int IndexOfNumber)>();
+
+    foreach (var digit in digits)
+    {
+        var indexOfNumber = calibrationValue.IndexOf(digit.Key, StringComparison.Ordinal);
+        if (indexOfNumber == -1) continue;
+        numbersWithIndexes.Add((digit.Value, indexOfNumber));
+    }
+
+    foreach (var digit in digits)
+    {
+        var indexOfNumber = calibrationValue.LastIndexOf(digit.Key, StringComparison.Ordinal);
+        if (indexOfNumber == -1) continue;
+        numbersWithIndexes.Add((digit.Value, indexOfNumber));
+    }
+
+    numbersWithIndexes.Sort((a, b) => a.IndexOfNumber.CompareTo(b.IndexOfNumber));
+
+    if (numbersWithIndexes.Count > 0)
+    {
+        var finalNumber = int.Parse($"{numbersWithIndexes[0].Value}{numbersWithIndexes[^1].Value}");
+        finalNumbers.Add((calibrationValue, finalNumber));
+    }
+}
+
+foreach (var finalNumber in finalNumbers)
+{
+    Console.Write(finalNumber);
+    Console.WriteLine();
+}
+
+Console.WriteLine(finalNumbers.Sum(x => x.FinalNumber));
